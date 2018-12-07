88f6ca07a935645a6fccc3e6df3521486ab43035
Louis pc <louis.chovaneck@gmail.com>
2018-12-07 02:43:44 +0100

    meteo ok

--- meteo.php
+++ meteo.php
@@ -6,6 +6,9 @@ $p=new WebPage("test",null,"img/favicon.png");
 $p->appendCssUrl("css/style1.css");
 $p->appendCssUrl("css/style.css");
 $p->appendCssUrl("css/icon_weather.css");
+$p->appendJsUrl("js/compass.js");
+$p->appendJsUrl("js/AjaxRequest.js");
+
 
 $p->appendContent(<<<HTML
 <br><br><br><br><br>
@@ -17,7 +20,7 @@ $p->appendContent(<<<HTML
 
           <div class="compass">
             <div class="direction">
-              <p><i id="dir">NE</i><span id="directionn"> 10 mph</span></p>
+              <p><i id="dir"></i><span id="directionn"></span></p>
             </div>
             <div id="fleche" class="arrow ne"></div>
           </div>
@@ -25,6 +28,9 @@ $p->appendContent(<<<HTML
         </div>
         <div class="col-sm-4">
           <p class="text-center" id="meteoactu">Météo actuelle : </p>
+          <p class="text-center" id="sous-titre"> Température : <span id="temp"></span></p>
+          <p class="text-center" id="sous-titre">Humidité : <span id="hum"></span></p>
+
           <p class="text-center">
           <div class="icon sun-shower">
             <div class="cloud"></div>
@@ -34,7 +40,7 @@ $p->appendContent(<<<HTML
             <div class="rain"></div>
           </div>
 
-          <div class="icon thunder-storm">
+          <div class="icon thunder-storm" id="orage">
             <div class="cloud"></div>
             <div class="lightning">
               <div class="bolt"></div>
@@ -42,12 +48,12 @@ $p->appendContent(<<<HTML
             </div>
           </div>
 
-          <div class="icon cloudy">
+          <div class="icon cloudy" id="cloud">
             <div class="cloud"></div>
             <div class="cloud"></div>
           </div>
 
-          <div class="icon flurries">
+          <div class="icon flurries" id="snow">
             <div class="cloud"></div>
             <div class="snow">
               <div class="flake"></div>
@@ -55,19 +61,66 @@ $p->appendContent(<<<HTML
             </div>
           </div>
 
-          <div class="icon sunny">
+          <div class="icon sunny"  id="sun">
             <div class="sun">
               <div class="rays"></div>
             </div>
           </div>
 
-          <div class="icon rainy">
+          <div class="icon rainy" id="pluie">
             <div class="cloud"></div>
             <div class="rain"></div>
           </div>
         </div>
         </p>
-    <div class="col-sm-4"></div>
+    <div class="col-sm-4">
+    <p class="text-center" id="meteoactu">Météo J+2 : </p>
+    <p class="text-center" id="sous-titre"> Date : <span id="date"></span></p>
+    <p class="text-center" id="sous-titre"> Température : <span id="tempj2"></span></p>
+    <p class="text-center" id="sous-titre">Humidité : <span id="humj2"></span></p>
+    <p class="text-center">
+          <div class="icon sun-shower">
+            <div class="cloud"></div>
+            <div class="sun">
+              <div class="rays"></div>
+            </div>
+            <div class="rain"></div>
+          </div>
+
+          <div class="icon thunder-storm" id="orage2">
+            <div class="cloud"></div>
+            <div class="lightning">
+              <div class="bolt"></div>
+              <div class="bolt"></div>
+            </div>
+          </div>
+
+          <div class="icon cloudy" id="cloud2">
+            <div class="cloud"></div>
+            <div class="cloud"></div>
+          </div>
+
+          <div class="icon flurries" id="snow2">
+            <div class="cloud"></div>
+            <div class="snow">
+              <div class="flake"></div>
+              <div class="flake"></div>
+            </div>
+          </div>
+
+          <div class="icon sunny"  id="sun2">
+            <div class="sun">
+              <div class="rays"></div>
+            </div>
+          </div>
+
+          <div class="icon rainy" id="pluie2">
+            <div class="cloud"></div>
+            <div class="rain"></div>
+          </div>
+        </div>
+        </p>
+    </div>
 </div>
 </div>
 
