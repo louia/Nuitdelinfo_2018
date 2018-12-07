3d2870f6b431f2dc4ea520c7105b21009fc7c344
Axel Undreiner <axel.undreiner@etudiant.univ-reims.fr>
2018-12-07 01:57:58 +0100

    Map ok

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    $(function () {
-        $(document).scroll(function () {
-          var $nav = $(".navbar");
-          $nav.toggleClass('scrolled', $(this).scrollTop() > 300);
-          $nav.toggleClass('non-scrolled', $(this).scrollTop() < 300);
-        });
-      });
-}
